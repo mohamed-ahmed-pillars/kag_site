12021bd7b23d7c25134e546bb19432bf63444702
Mohammed-doit_esky-rnd <mohamed.abdelmaqsoud@doit.com.eg>
2026-04-13 07:17:26 +0200

    chore: export ThemeProvider from layout barrel

---
+++ frontend/components/layout/index.ts
@@ -0,0 +1,8 @@
+export { Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as LanguageSwitcher } from './LanguageSwitcher';
+export { default as MobileMenu } from './MobileMenu';
+export { default as PageTransition } from './PageTransition';
+export { default as ScrollToTop } from './ScrollToTop';
+export { default as WhatsAppButton } from './WhatsAppButton';
+export { ThemeProvider } from './ThemeProvider';
